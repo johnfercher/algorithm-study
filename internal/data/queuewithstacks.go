4d62fcc6c4f93dc0ebfaf62693adea5304cfcfae
Johnathan Fercher <johnathanfercher22@gmail.com>
2022-01-29 20:31:50 -0300

    Queue using stacks

---
+++ internal/data/queuewithstacks.go
@@ -0,0 +1,57 @@
+package data
+
+type QueueWithStack struct {
+	stack1, stack2 *StackIntLinkedList
+	length         int
+}
+
+func NewQueueWithStack() *QueueWithStack {
+	return &QueueWithStack{
+		stack1: NewStackIntLinkedList(),
+		stack2: NewStackIntLinkedList(),
+	}
+}
+
+func (s *QueueWithStack) Length() int {
+	return s.length
+}
+
+func (s *QueueWithStack) Enqueue(value int) {
+	s.length++
+	for {
+		valueInStack, ok := s.stack1.Pop()
+		if !ok {
+			break
+		}
+
+		s.stack2.Push(valueInStack)
+	}
+
+	s.stack1.Push(value)
+
+	for {
+		valueInStack, ok := s.stack2.Pop()
+		if !ok {
+			break
+		}
+
+		s.stack1.Push(valueInStack)
+	}
+}
+
+func (s *QueueWithStack) Dequeue() (int, bool) {
+	if s.length == 0 {
+		return 0, false
+	}
+
+	s.length--
+	return s.stack1.Pop()
+}
+
+func (s *QueueWithStack) First() (int, bool) {
+	return s.stack1.Peek()
+}
+
+func (s *QueueWithStack) Print() {
+	s.stack1.Print()
+}
---
+++ internal/data/queuewithstacks_test.go
@@ -0,0 +1,114 @@
+package data_test
+
+import (
+	"algorithm-study/internal/data"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewQueueWithStack(t *testing.T) {
+	// Act
+	sut := data.NewQueueWithStack()
+
+	// Assert
+	assert.Zero(t, sut.Length())
+	value, ok := sut.First()
+	assert.Zero(t, value)
+	assert.False(t, ok)
+}
+
+func TestQueueWithStack_Enqueue(t *testing.T) {
+	// Arrange
+	sut := data.NewQueueWithStack()
+
+	value := 0
+	ok := false
+
+	// Enqueue 1
+	sut.Enqueue(1)
+	value, ok = sut.First()
+	assert.Equal(t, 1, value)
+	assert.True(t, ok)
+	assert.Equal(t, 1, sut.Length())
+
+	// Enqueue 2
+	sut.Enqueue(2)
+	value, ok = sut.First()
+	assert.Equal(t, 1, value)
+	assert.True(t, ok)
+	assert.Equal(t, 2, sut.Length())
+
+	// Enqueue 3
+	sut.Enqueue(3)
+	value, ok = sut.First()
+	assert.Equal(t, 1, value)
+	assert.True(t, ok)
+	assert.Equal(t, 3, sut.Length())
+}
+
+func TestQueueWithStack_Dequeue(t *testing.T) {
+	// Arrange
+	sut := data.NewQueueWithStack()
+
+	value := 0
+	ok := false
+
+	sut.Enqueue(1)
+	sut.Enqueue(2)
+	sut.Enqueue(3)
+
+	sut.Print()
+
+	// Dequeue 1
+	value, ok = sut.Dequeue()
+	assert.Equal(t, 1, value)
+	assert.True(t, ok)
+	value, ok = sut.First()
+	assert.Equal(t, 2, value)
+	assert.True(t, ok)
+	assert.Equal(t, 2, sut.Length())
+
+	// Dequeue 2
+	value, ok = sut.Dequeue()
+	assert.Equal(t, 2, value)
+	assert.True(t, ok)
+	value, ok = sut.First()
+	assert.Equal(t, 3, value)
+	assert.True(t, ok)
+	assert.Equal(t, 1, sut.Length())
+
+	// Dequeue 3
+	value, ok = sut.Dequeue()
+	assert.Equal(t, 3, value)
+	assert.True(t, ok)
+	value, ok = sut.First()
+	assert.Zero(t, value)
+	assert.False(t, ok)
+	assert.Equal(t, 0, sut.Length())
+
+	// Invalid
+	value, ok = sut.Dequeue()
+	assert.Zero(t, value)
+	assert.False(t, ok)
+	value, ok = sut.First()
+	assert.Zero(t, value)
+	assert.False(t, ok)
+	assert.Equal(t, 0, sut.Length())
+}
+
+func TestQueueWithStack_Peek(t *testing.T) {
+	// Arrange
+	sut := data.NewQueueWithStack()
+
+	value := 0
+	ok := false
+
+	sut.Enqueue(1)
+	sut.Enqueue(2)
+	sut.Enqueue(3)
+
+	// Enqueue 1
+	value, ok = sut.First()
+	assert.Equal(t, 1, value)
+	assert.True(t, ok)
+}
